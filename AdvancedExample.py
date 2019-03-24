0282066fd18fa1d732fbdd8c2c0b0e63e03fa40b
Ahmed Soliman <=>
2019-03-24 14:50:32 +0100

    crawl political journalists from twitter

---
+++ AdvancedExample.py
@@ -0,0 +1,26 @@
+import json
+from Crawler import Crawler
+import logging
+import multiprocessing as mp
+
+def crawl(username):
+	crawler = Crawler(operating_system = "mac")
+	crawler.crawl_with_username(username)
+	crawler.destroy()
+
+
+# set logging setting
+logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
+# init twitter crawler
+usernames = []
+
+# get usernames political journalists on twitter
+with open("political_authors/statsocial.json","r") as f:
+	journalists = json.load(f)
+	# iterate over all journalists
+	for journalist in journalists:
+		usernames.append(journalist['username'].split("@")[-1])
+
+p = mp.Pool(8)
+
+p.map(crawl,usernames)
